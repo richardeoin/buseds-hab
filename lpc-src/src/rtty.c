99d15566b40ed9cd79e752369cbbbd252fb231bc
Richard Eoin Meadows <richardeoin@gmail.com>
2013-11-22 18:07:47 +0000

    Added RTTY and associated test

---
+++ lpc-src/src/rtty.c
@@ -0,0 +1,142 @@
+/* 
+ * Bit-bangs RTTY
+ * Copyright (C) 2013  richard
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "LPC11xx.h"
+
+/**
+ * Interface to the physical world
+ */
+#ifndef RTTY_TEST
+
+#define RTTY_ACTIVATE()
+#define RTTY_DEACTIVATE()
+#define RTTY_SET(b)		
+#define RTTY_NEXT()
+
+#else
+
+#include <stdio.h>
+#define RTTY_ACTIVATE()
+#define RTTY_DEACTIVATE()
+#define RTTY_SET(b)		printf("%d", b & 1)
+#define RTTY_NEXT()		printf("\n");
+
+#endif
+
+/**
+ * Formatting 8N2
+ */
+#define ASCII_BITS	8
+#define BITS_PER_CHAR	11
+
+/**
+ * Where we currently are in the rtty output byte
+ *
+ * 0 = Start Bit
+ * 1-8 = Data Bit
+ * 10 = Stop Bit
+ * 11 = Stop Bit
+ */
+uint8_t rtty_phase;
+/**
+ * Where we are in the current output string
+ */
+uint32_t rtty_index;
+
+/**
+ * Details of the string that is currently being output
+ */
+char* rtty_string;
+uint32_t rtty_string_length = 0;
+
+/**
+ * Returns 1 if we're currently outputting.
+ */
+int rtty_active(void) {
+  return (rtty_string_length > 0);
+}
+
+/**
+ * Sets an output string.
+ *
+ * Returns 0 on success, 1 if a string is already active.
+ */
+int rtty_set_string(char* string, uint32_t length) {
+  if (!rtty_active()) {
+    // Copy
+    rtty_string = string;
+    rtty_string_length = length;
+    // Initialise
+    rtty_index = 0;
+    rtty_phase = 0;
+
+    return 0; // Success
+  } else {
+    return 1; // Already active
+  }
+}
+
+/**
+ * Called at the baud rate, outputs bits of rtty
+ */
+void rtty_tick(void) {
+  if (rtty_active()) {
+    RTTY_ACTIVATE();
+
+    if (rtty_phase == 0) { // Start
+      // Low
+      RTTY_SET(0);
+    } else if (rtty_phase <= ASCII_BITS) {
+      // Data
+      RTTY_SET(rtty_string[rtty_index] >> (rtty_phase - 1));
+    } else if (rtty_phase <= BITS_PER_CHAR) { // Stop 1
+      // High
+      RTTY_SET(1);
+    }
+
+    rtty_phase++;
+
+    if (rtty_phase >= BITS_PER_CHAR) { // Next character
+      rtty_phase = 0; rtty_index++; RTTY_NEXT();
+
+      if (rtty_index >= rtty_string_length) { // All done, deactivate
+	rtty_string_length = 0; // Deactivate
+
+	RTTY_DEACTIVATE();
+      }
+    }
+  }
+}
+
+
+#ifdef RTTY_TEST
+
+int main() {
+  rtty_set_string("RTTY", 4);
+  while (rtty_active()) {
+    rtty_tick();
+  }
+}
+
+#endif
